4d2e90732087ced99ea73eb061965cf7fa98158e
rheast <chen@rheast.com>
2022-07-10 16:33:30 +0800

    Add files via upload

--- math.py
+++ math.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
 H = {'gag','pol','env'}
 I = {'ENSE00003838363','ENSE00001316091'}
-I.update(H)
 A = {'androgen','androgen receptor'}
 def f(x):
     if x == A:
-        return I
+        return I | H
 print(H <= f(A)) #True
 print(H == f(A)) #False
